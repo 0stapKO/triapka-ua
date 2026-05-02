9df7ef260635b7427499b3e416bc19182054fe05
0stapKO <ostap22lviv@gmail.com>
2026-05-02 23:39:25 +0300

    created service interface

---
+++ src/Triapka.Application/DTOs/ProductDetailsDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triapka.Application.DTOs;
+public class ProductDetailsDto
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public double Price { get; set; }
+
+    public List<string> ImageUrls { get; set; } = [];
+
+    public string CategoryName { get; set; } = null!;
+
+    public float Rate { get; set; }
+}
---
+++ src/Triapka.Application/DTOs/ProductListDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triapka.Application.DTOs;
+public class ProductListDto
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public double Price { get; set; }
+
+    public string MainImageUrl { get; set; } = null!;
+}
