cf898ad617d0bfb777a848997111b66b6f5b8387
Ostap Kutko <159272119+0stapKO@users.noreply.github.com>
2026-05-05 23:35:57 +0300

    added CartRepository and WishlistRepository (#33)
    * added CartRepository and WishlistRepository
    
    * copilot fixes

--- src/Triapka.Application/Interfaces/ICartRepository.cs
+++ src/Triapka.Application/Interfaces/ICartRepository.cs
@@ -3,9 +3,9 @@
 namespace Triapka.Application.Interfaces;
 public interface ICartRepository
 {
-    Task<Cart> GetCartByUserIdAsync(int userId);
+    Task<Cart?> GetCartByUserIdAsync(int userId);
 
     Task<Cart> AddItemAsync(CartItem item);
 
-    Task<Cart> RemoveItemAsync(int itemId);
+    Task<Cart?> RemoveItemAsync(int itemId);
 }
